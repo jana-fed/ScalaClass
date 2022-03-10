eeef9bb593cf33da388927133964580bf41f6f91
Jana <jana.pukach@gmail.com>
2022-03-10 21:06:45 +0200

    Day 6

---
+++ src/main/scala/Day6ExerciseCityPopulation.scala
@@ -0,0 +1,53 @@
+object Day6ExerciseCityPopulation extends App {
+//first task
+  def farengeit (celsius: Double): Unit ={
+
+    println(32+celsius*9/5)
+  }
+  farengeit(36.6)
+  farengeit(37)
+  farengeit(48.5)
+
+ //second task
+  println("Function to calculate city growth")
+  //TODO write a function getCityYear which takes the following 4 parameters
+  //p0: Int - how many people are in beginning
+  //perc: Int - yearly growth rate in percentages
+  //delta: Int - how many people immigrate(+)/emigrate to the city each year
+  //targetPopulation: Int - population we want to reach
+
+  //we want to return the year city will reach targetPopulation
+  //or we return -1 if the city will NEVER reach the population
+  //it is a little bit tricky because we do not want to use return statements
+  //we want to return the last line only
+  //so you probably want to use some variable to hold the result
+  //you will need a loop - while probably
+  //you will need some if else statements
+  //if we write comments /**
+  //those are so called ScalaDoc style comments and they can be used by automatic tools to create documentation
+
+  // */
+
+  /**
+   * getCityYear
+   * @param p0 - original City population
+   * @param percentage - yearly growth rate in percentages
+   * @param delta - how many people immigrate(+)/emigrate to the city each year
+   * @param targetPopulation - population we want to reach
+   * @return number of years to reach , -1 if not reachable
+   */
+  def getCityYear(p0: Int, percentage: Double, delta: Int, targetPopulation: Int):Unit = {
+    var years = 2022
+    while (p0 <= targetPopulation ) {
+      val p1 = p0+(p0 * (percentage / 100) + delta)
+      p0 == p1
+      years += 1
+    }
+ ///   9000 //FIXME //right now it returns this 9000 all the time
+  }
+
+  println(getCityYear(1000,2,50,1200)) // should print 3
+  println(getCityYear(1000,2,-50,1200)) // should print -1
+  println(getCityYear(1500000,2.5,10000,2000000)) // should print 10
+
+}
