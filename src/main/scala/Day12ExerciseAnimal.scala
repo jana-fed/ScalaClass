aace8ea9e092b86063598d502741f5e8e16d29b8
Jana <jana.pukach@gmail.com>
2022-03-29 17:20:55 +0300

    Day11 pangram exercise

---
+++ src/main/scala/Day12ExerciseAnimal.scala
@@ -0,0 +1,34 @@
+//TODO add class variables name, animalType, likes, sound- all strings and age which is Int
+class animal(val nickname: String, 
+             val animalType: String,
+             val likes: String,
+             val sound: String,
+             val age: Int) {
+ //TODO add class method makeSound which prints sound
+    def makeSound(): Unit = {
+    println(s"What's does a $animalType says? It says $sound!")
+    }
+//BONUS
+//TODO add class method meet which has a parameter of contact:String
+//TODO this meet should decide whether to greet if they like the contact
+  //TODO to run away if they do not like the contact
+    def meet(contact: String): Unit ={
+    if (contact == "like") println(s"$nickname likes you and says HI! $sound!")
+    else println(s"Oh no! $nickname runned away!")
+    }
+}
+object Day12ExerciseAnimal extends App {
+
+  val catMuris = new animal(nickname = "Muris", animalType = "cat", likes = "Fish", sound = "Mau-Mau", age = 5 )
+  val dogPedro = new animal(nickname = "Pedro", animalType = "dog", likes = "bones", sound = "Vau-Vau", age = 7)
+  val catGven = new animal(nickname = "Venja", animalType = "cat", likes = "mouse", sound = "Mjek", age = 9)
+  val parrotAnna  = new animal(nickname = "Njushek", animalType = "parrot", likes = "seeds", sound = "Chiv-Chiv", age = 7)
+  parrotAnna.makeSound
+  dogPedro.makeSound
+  catMuris.makeSound
+  catMuris.meet("like")
+  parrotAnna.meet("not like")
+  //TODO create some animals objects from Animal class with appropriate properties, dog, cat, mouse etc :)
+  //TODO call makeSound method
+  //TODO call meet method
+}
