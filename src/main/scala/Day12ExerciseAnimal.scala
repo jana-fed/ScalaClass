051383974cdd845b018056b323951076c1f5fb15
Jana <jana.pukach@gmail.com>
2022-03-29 17:21:49 +0300

    Day 12 animal exercise

--- src/main/scala/Day12ExerciseAnimal.scala
+++ src/main/scala/Day12ExerciseAnimal.scala
@@ -31,4 +31,5 @@ object Day12ExerciseAnimal extends App {
   //TODO create some animals objects from Animal class with appropriate properties, dog, cat, mouse etc :)
   //TODO call makeSound method
   //TODO call meet method
+
 }
