ee80848eeddf45688bf654e889f38ba61800db65
Jana <jana.pukach@gmail.com>
2022-04-03 10:01:57 +0300

    Day 13 Songs Exercise with Rap songs

---
+++ src/main/scala/Flower.scala
@@ -0,0 +1,6 @@
+class Flower(flowerType:String, val color:String) {
+  println(s"Created a new flower of type: $flowerType with color $color")
+  //more methods here
+  //more construction work here
+}
+
