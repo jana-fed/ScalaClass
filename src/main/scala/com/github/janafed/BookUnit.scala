be7a18ab258a67c449226ed66edf0fa45b71ad2f
Jana <jana.pukach@gmail.com>
2022-04-28 18:12:04 +0300

    Day 22 exercise

---
+++ src/main/scala/com/github/janafed/BookUnit.scala
@@ -0,0 +1,4 @@
+package com.github.janafed
+
+case class BookUnit(id:String, author:String, title:String, genre:String, price:Double, publish_date:String, description: String)
+//TODO finish the case class with the right fields from books.xml book elemen
