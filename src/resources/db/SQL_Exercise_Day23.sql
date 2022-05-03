05a3b894c38a51a5a4e1b880219d9115896aea2a
Jana <jana.pukach@gmail.com>
2022-05-03 19:41:52 +0300

    Day 24 SQL exercise

--- src/resources/db/SQL_Exercise_Day23.sql
+++ src/resources/db/SQL_Exercise_Day23.sql
@@ -2,3 +2,12 @@ select * from customers c where country != "USA";
 select * from customers c where country = 'Germany' ;
 select * from employees e where title like "%Sales%Agent%";
 select DISTINCT BillingCountry  from invoices i ;
+SELECT a.Title Album, 
+a2.Name Artist, t.Name SongName, t.GenreId ,g.Name 
+FROM tracks t
+JOIN albums a 
+ON t.AlbumId = a.AlbumId
+JOIN artists a2 
+ON a.ArtistId = a2.ArtistId
+JOIN genres g 
+ON t.GenreId = g.GenreId  ;
